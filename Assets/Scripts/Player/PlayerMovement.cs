01f67f03cfc7eedac1f38729532fdb1388c8ae4c
unqnq <140976693+unqnq@users.noreply.github.com>
2026-03-26 16:51:21 +0200

    Player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float coef = 2;
     void Start()
     {
-        
+        playerTransform = transform.Find("PlayerModel");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float halfScreen = Screen.width / 2;
+        float xPos = (Input.mousePosition.x - halfScreen) / halfScreen;
+        float finalXPos = Mathf.Clamp(xPos*coef, -coef, coef);
+        playerTransform.localPosition = new Vector3(finalXPos, playerTransform.transform.position.y, playerTransform.transform.position.z);
     }
 }
